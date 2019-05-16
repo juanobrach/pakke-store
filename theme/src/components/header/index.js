ab70e9ebbd1c153f478b148b623d3b1f4e1a6b75
juano <juanobrach@gmail.com>
2019-05-16 18:27:16 -0300

    agrego padding a elementos del header

--- theme/src/components/header/index.js
+++ theme/src/components/header/index.js
@@ -194,7 +194,7 @@ export default class Header extends React.Component {
 															</div>
 														</div>}
 						</div>
-						<div className="columns is-gapless" style={{alignItems:'flex-end', justifyContent:'space-between', marginTop:'.9vw'}}>
+						<div className="columns is-gapless" style={{alignItems:'flex-end', justifyContent:'space-between', marginTop:'.9vw', paddingBottom:'10px'}}>
 							<div className="column is-9">
 								<div className="primary-nav is-hidden-mobile" style={{lineHeight:' 3px'}}>
 									<HeadMenu
