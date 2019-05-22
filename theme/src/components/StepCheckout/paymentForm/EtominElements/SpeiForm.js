933d78e63af9e058c833bee8f6c38971d28dde97
juano <juanobrach@gmail.com>
2019-05-22 18:32:16 -0300

    mensajes en oxxo y spei

--- theme/src/components/StepCheckout/paymentForm/EtominElements/SpeiForm.js
+++ theme/src/components/StepCheckout/paymentForm/EtominElements/SpeiForm.js
@@ -14,6 +14,7 @@ class SpeiForm extends React.Component {
     const submit = this.submit;
     return (
       <form onSubmit={handleSubmit} >
+        <p style={{backgroundColor:'#623a8a', padding:'2em', color:'white'}} >Transferencia SPEI es el sistema de pagos electrónicos interbancarios desarrollado por banco de México, una vez completada la orden podra ver el codigo de transferencia para finalizar la compra.</p>
         <div className="columns is-marginless is-vcentered" style={{position:'absolute', left:'0', right:'0', width:'100%'}}>
           <div className="column">
             <button onClick={handleBack} className="back-button">Cancelar</button>  
